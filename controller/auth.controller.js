254e2c49772759c2e59d45584beec595106d9ad5
Roua <rouahammami14@gmail.com>
2026-03-10 11:12:58 +0100

    adding backend v4

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -78,7 +78,9 @@ export const signin = async (req, res, next) => {
 
     const { password: pass, ...rest } = validUser._doc
 
-    res.status(200).cookie("access_token", token, { httpOnly: true }).json(rest)
+    res.status(200).cookie("access_token", token, {  httpOnly: true,
+      secure: true,       // ✅ must be HTTPS on Render
+      sameSite: "none",  }).json(rest)
   } catch (error) {
     next(error)
   }
@@ -144,7 +146,11 @@ export const uploadImage = async (req, res, next) => {
 export const signout = async (req, res, next) => {
   try {
     res
-      .clearCookie("access_token")
+        .clearCookie("access_token", { 
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+  })
       .status(200)
       .json("User has been loggedout successfully!")
   } catch (error) {
